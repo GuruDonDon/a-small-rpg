1ec8c69d0cd8894757574306c572cbbf2d4226b1
GuruDonDon <GuruDonDon@github.com>
2018-11-19 23:05:11 +0100

    Some minor planing

--- a-small-rpg/a-small-rpg/a-small-rpg.cpp
+++ a-small-rpg/a-small-rpg/a-small-rpg.cpp
@@ -1,21 +1,18 @@
-// a-small-rpg.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	//Creat new map
+
+	/*Game loop
+		while game running
+		ask for use input
+		redraw map
+		check if game over -> game = not running
+	*/
+
+	//restart game?
+
+	std::cout << "Program ran to the end"; // remove later
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
